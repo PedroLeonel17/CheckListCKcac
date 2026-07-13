82f51a978856e1abb2ccc13f6591c526d527ad49
Meiryanne Martins <meirympereira@gmail.com>
2026-07-13 19:33:14 -0300

    feat: add cross origin

--- src/main/java/dev/pedro/CodigoKidChecklist/Controller/ChecklistController.java
+++ src/main/java/dev/pedro/CodigoKidChecklist/Controller/ChecklistController.java
@@ -5,11 +5,13 @@ import dev.pedro.CodigoKidChecklist.Dto.ChecklistRespDto;
 import dev.pedro.CodigoKidChecklist.Services.ChecklistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/checklist") // URL base para tudo relacionado a alunos
 public class ChecklistController{
 
